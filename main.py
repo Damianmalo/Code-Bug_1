0c1f36f5fc27011941689fc39adc82657c08b13e
Damianmalo <Damian.malo12@gmail.com>
2024-08-29 11:38:53 -0400

    Add files via upload
    first code bug

---
+++ main.py
@@ -0,0 +1,17 @@
+name= "Georgia"
+age = "3.5"
+hair_color = "brown"
+
+print(name)
+
+def hair_color_function():
+  if hair_color == "yellow": 
+      print("You've got yellow")
+  else:
+      print("You don't have yellow hair")
+      
+
+hair_color_function()
+
+sentencemerge = name + " " + "is" + " " + age 
+print(sentencemerge)
